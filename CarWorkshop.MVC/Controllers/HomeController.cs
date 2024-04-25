2685f735066a235bb050bfbe856a53d301b59c01
Pablogp102 <pablopg102@gmail.com>
2024-04-25 22:01:51 +0200

    Entities, projects

--- CarWorkshop.MVC/Controllers/HomeController.cs
+++ CarWorkshop.MVC/Controllers/HomeController.cs
@@ -1,34 +1,58 @@
 using System.Diagnostics;
-
+using Microsoft.AspNetCore.Mvc;
 using CarWorkshop.MVC.Models;
 
-using Microsoft.AspNetCore.Mvc;
+namespace CarWorkshop.MVC.Controllers;
 
-namespace CarWorkshop.MVC.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
+    public IActionResult About()
+    {
+        var model = new About()
         {
-            return View();
-        }
+            Title = "CarWorkshop application",
+            Description = "Some description",
+            Tags = new List<string>() { "car", "app", "free" }
+        };
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        return View(model);
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+    public IActionResult Privacy()
+    {
+        var model = new List<Person>()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            new Person()
+            {
+                FirstName = "Jakub",
+                LastName = "Kozera"
+            },
+            new Person()
+            {
+                FirstName = "Adam",
+                LastName = "Ma³ysz"
+            },
+
+        };
+
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
